5c3cb767486def6cf804faafbc89630a19953620
afife99 <istanbul150414@gmail.com>
2023-05-09 12:44:48 +0300

    guncel

--- script.js
+++ script.js
@@ -44,4 +44,24 @@ function mesajGonder(){
             document.getElementById("icerik").scrollTop += 9999; /* yazdıkça scroolda gelsin */
         }
 });
+function getIp(callback) {
+    fetch('https://ipinfo.io/json?token=<your token>', { headers: { 'Accept': 'application/json' }})
+      .then((resp) => resp.json())
+      .catch(() => {
+        return {
+          country: 'us',
+        };
+      })
+      .then((resp) => callback(resp.country));
+   }
+   
+     const phoneInputField = document.querySelector("#phone");
+     
+     const phoneInput = window.intlTelInput(phoneInputField, {
+       preferredCountries: ["tr","us", "co", "in", "de"],
+       geoIpLookup: getIp,
+       utilsScript:
+         "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+     });
+
 
